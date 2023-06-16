9918ddb952565092de6fc481da6edea28be0d0ca
Carlos <deleonc72@gmail.com>
2023-06-16 15:04:37 -0600

    Se agregan el array para obtener datos de estadios

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  const personas = [
+    { nombre: 'Camp Nou', equipo: 'FC Barcelona' , foto: 'https://www.fcbarcelona.com/photo-resources/2021/08/09/276ad270-e5c6-453d-8d9f-212417ad7cb3/Camp-Nou-3.jpg?width=1200&height=750'},
+    { nombre: 'Santiago Bernabeu', equipo: 'Real Madrid CF' , foto:'https://media.cntraveler.com/photos/5e66a338b93f0800081a6f28/16:9/w_2560,c_limit/santiago-bernabe%CC%81u-stadium-madrid-2020-GettyImages-1194733745.jpg' },
+    { nombre: 'Stamford Bridge', equipo: 'Chelsea Football Club' , foto:'https://www.thesun.co.uk/wp-content/uploads/2023/03/2023-general-view-inside-stadium-804206041.jpg' }
+  ];
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
